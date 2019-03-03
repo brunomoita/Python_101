d2da6e75cd29c7f2accc92fbf8862b4839009050
brunomoita <brunosimoes@gmail.com>
2019-03-03 12:48:19 -0300

    Comments added

--- PythonTestProject/src/BreakTime.py
+++ PythonTestProject/src/BreakTime.py
@@ -3,16 +3,16 @@ Created on 3 de mar de 2019
 
 @author: bruno
 '''
-import time
-import webbrowser
+import time #Load the Time Lib
+import webbrowser # Load the webbrowser lib
 
-total_break = 3
-break_count = 0
+total_break = 3 #Total number of desired breaks
+break_count = 0 #Counter for the number of breaks
 
 
-print("This program started on"+time.ctime())
+print("This program started on"+time.ctime()) #Program start time
 
-while(break_count < total_break):
-   time.sleep(7200)
-   webbrowser.open("https://www.youtube.com/watch?v=fJ9rUzIMcZQ")
-   break_count = break_count + 1
+while(break_count < total_break): #Loop if the number of breaks is smaller the the number of desired breaks
+   time.sleep(7200) #The video will start every 2 hours in seconds
+   webbrowser.open("https://www.youtube.com/watch?v=fJ9rUzIMcZQ") #Open Bohemian Raspody on YouTube
+   break_count = break_count + 1 #Increase the number of breaks by 1 on each loop
