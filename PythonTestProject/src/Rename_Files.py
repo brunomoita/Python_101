5333563bc1454b49c96552f0c73e0b3d30188128
brunomoita <brunosimoes@gmail.com>
2019-03-04 02:25:19 -0300

    Working version for practice software to remove number from file names in specific folder

--- PythonTestProject/src/Rename_Files.py
+++ PythonTestProject/src/Rename_Files.py
@@ -9,13 +9,15 @@ def rename_files():
     file_list = os.listdir(r"C:\Py_Practice\Prank\prank\prank") #List the files on the folder, r is for raw interpretation of the dir path
     print(file_list)
     saved_path = os.getcwd()
-    print("The current directory is"+ saved_path)
+    print("The current directory is " + saved_path)
     os.chdir(r"C:\Py_Practice\Prank\prank\prank")
     
     #2 Rename all the files listed removing the number from the file names
     for file_name in file_list:
-        print("Old file name -"+ file_name)
-        print("New file name -"+ file_name.strip('0123456789'))
-        os.rename(file_name, file_name.strip('0123456789'))
+        print("Old file name -" + file_name)
+        print("New file name -" + file_name.strip("0123456789"))
+        os.chdir(r"C:\Py_Practice\Prank\prank\prank")
+        print("The current directory is " + saved_path)
+        os.rename(file_name, file_name.strip("0123456789"))
         os.chdir(saved_path)
 rename_files()
