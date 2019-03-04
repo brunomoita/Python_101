fff821a11a3328ebfaefa51ed42760e310c0be43
brunomoita <brunosimoes@gmail.com>
2019-03-04 20:42:54 -0300

    WOrking trutle drawing a square, a circle and a triangle.

--- PythonTestProject/src/Geometry.py
+++ PythonTestProject/src/Geometry.py
@@ -5,25 +5,22 @@ Created on 4 de mar de 2019
 '''
 import turtle
 
-
-
 def draw_square():
-    square_count = 0
+    window = turtle.Screen()
+    window.bgcolor("green")
     
-    while square_count <4:
-        window = turtle.Screen()
-        window.bgcolor("green")
+    chuck = turtle.Turtle()
+    chuck.shape("circle")
+    chuck.color("white")
+    chuck.speed(1)
     
-        chuck = turtle.Turtle()
-        chuck.shape("circle")
-        chuck.color("white")
-        chuck.speed(1)
-        
+    square = 0
+    
+    while square < 4:    
         chuck.forward(100)
         chuck.right(90)
-    
-        square_count = square_count + 1        
-
+        square = square + 1
+                
 def draw_circle():        
     window = turtle.Screen()
     window.bgcolor("green")
@@ -34,20 +31,21 @@ def draw_circle():
     leia.circle(100)
        
 def draw_triangle():
-    triangle_count = 0
+    window = turtle.Screen()
+    window.bgcolor("green")
     
-    while triangle_count < 3:
-        window = turtle.Screen()
-        window.bgcolor("green")
+    jason = turtle.Turtle()
+    jason.shape("arrow")
+    jason.color("black")
     
-        jason = turtle.Turtle()
-        jason.shape("arrow")
-        jason.color("black")
+    triangle = 0
+    
+    while triangle < 3:
         jason.forward(100)
-        jason.right(135)
-        
-        triangle_count = triangle_count + 1
-        
-draw_square()
+        jason.right(120)
+        triangle = triangle + 1
+    window.exitonclick()
+    
+draw_square()     
 draw_circle()
 draw_triangle()
